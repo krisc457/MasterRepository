4cb8376ac441b4cb15d0c41f22b2cf4fb81e6e99
krisc457 <kristoffer.schmidt@hotmail.com>
2017-05-12 09:45:05 +0200

    safe commit

--- src/main/java/hello/GameController.java
+++ src/main/java/hello/GameController.java
@@ -24,7 +24,7 @@ public class GameController {
     MajorNation usa = new MajorNation("USA");
     MajorNation japan = new MajorNation("JAPAN");
     MajorNation russia = new MajorNation("RUSSIA");
-    
+
     @GetMapping("/map")
     public ModelAndView map(HttpSession session) {
         initMajorNationsList();
